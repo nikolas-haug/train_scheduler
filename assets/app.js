1280e059f816fddb46050625afc26bec6df48e8f
Nikolas  Haug <nikolashaug@er-10-129-72-253.wireless.umn.edu>
2018-04-26 21:11:17 -0500

    add new firebase time functionality

--- assets/app.js
+++ assets/app.js
@@ -14,6 +14,59 @@ $('document').ready(function() {
 
   var database = firebase.database();
 
+  function append(childSnapshot) {
+    //set the current time in the heading
+    var currentTime = moment();
+    console.log("the current time is: " + moment(currentTime).format("HH:mm"));
+
+    var name = childSnapshot.val().name;
+    var destination = childSnapshot.val().destination;
+    var trainFirstTime = childSnapshot.val().trainFirstTime;
+    var frequency = childSnapshot.val().frequency;
+
+    //go back one year with the train's first departure time
+    var firstTimeConverted = moment(trainFirstTime, "HH:mm").subtract(1, "years");
+    //format the converted time for display - CHECK THIS IS NECESSARY
+    var trainTime = moment(firstTimeConverted).format("HH:mm");
+    //calculate the difference between now and the first train
+	var timeDifference = moment().diff(moment(firstTimeConverted), 'minutes');
+    //use modulo to check for the remainder between difference and train frequency
+    var timeRemainder = timeDifference % frequency;
+	//calculate minutes till next traiin
+	var minsAway = frequency - timeRemainder;
+	//create variable for next train's arrival time
+	var nextTrain = moment().add(minsAway, 'minutes');
+    // console.log("ARRIVAL TIME: " + moment(nextTrain).format('HH:mm A'));
+    
+    //append the data to the DOM from the database
+    $('#trainData').append(
+            `<tr class="train-item"><td>${ childSnapshot.val().name }</td><td>${ childSnapshot.val().destination }</td><td>${ childSnapshot.val().frequency }</td><td>${ moment(nextTrain).format('HH:mm A') }</td><td>${ minsAway }</td>`
+        )
+
+    $('#current-time').text(moment(currentTime).format("HH:mm A"));
+
+   }
+
+   function syncData() {
+       //clear table
+        $('#trainData').empty();
+       //call append function from database
+       console.log(database.ref());
+       database.ref().once('value').then(function(snapshot) {
+            var val = snapshot.val();
+            var keys = Object.keys(val);
+            console.log(val);
+            console.log(keys);
+            keys.forEach(function(key) {
+                database.ref(key).once('value').then(append);
+            });
+       });
+       
+   }
+
+   setInterval(syncData, 60000);
+//    setTimeout(syncData, 1000);
+
   $('#newTrain').on('click', function(event) {
       event.preventDefault();
         
@@ -38,41 +91,10 @@ $('document').ready(function() {
 
   });
 
-  database.ref().on('child_added', function(childSnapshot) {
-    //set the current time in the heading
-    var currentTime = moment();
-    console.log("the current time is: " + moment(currentTime).format("HH:mm"));
-
-    var name = childSnapshot.val().name;
-    var destination = childSnapshot.val().destination;
-    var trainFirstTime = childSnapshot.val().trainFirstTime;
-    var frequency = childSnapshot.val().frequency;
-
-    //go back one year with the train's first departure time
-    var firstTimeConverted = moment(trainFirstTime, "HH:mm").subtract(1, "years");
-    //format the converted time for display
-    var trainTime = moment(firstTimeConverted).format("HH:mm");
-    //calculate the difference between now and the first train
-	var timeDifference = moment().diff(moment(firstTimeConverted), 'minutes');
-    //use modulo to check for the remainder between difference and train frequency
-    var timeRemainder = timeDifference % frequency;
-	//calculate minutes till next traiin
-	var minsAway = frequency - timeRemainder;
-	//create variable for next train's arrival time
-	var nextTrain = moment().add(minsAway, 'minutes');
-    // console.log("ARRIVAL TIME: " + moment(nextTrain).format('HH:mm A'));
-    
-    //append the data to the DOM from the database
-    $('#trainData').append(
-            `<tr class="train-item"><td>${ childSnapshot.val().name }</td><td>${ childSnapshot.val().destination }</td><td>${ childSnapshot.val().frequency }</td><td>${ moment(nextTrain).format('HH:mm A') }</td><td>${ minsAway }</td>`
-        )
-
-    $('#current-time').text(moment(currentTime).format("HH:mm A"));
-
+database.ref().on('child_added', append, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
   });
-
-    
-
+  //refresh the train times schedule - CHECK THAT ONLY THAT REFRESHES, NOT THE ENTIRE PAGE
   $('#refresh-schedule').on('click', function() {
     location.reload();
   });
